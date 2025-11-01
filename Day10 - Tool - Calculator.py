6e2cbe204582bfdc0c04a0b3e81f9f1b441a46a5
Henrique <depaolibio@gmail.com>
2025-10-31 17:00:35 -0700

    Add files via upload
    A simple calculator with the 4 basic operations (add, subtract, multiply, and divide).

---
+++ Day10 - Tool - Calculator.py
@@ -0,0 +1,59 @@
+art = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(art)
+    loop = True
+    n1 = float(input("Type the first number: "))
+
+    while loop == True:
+        for symbol in operations:
+            print(symbol)
+        operator = input("Type the operator : ")
+        n2 = float(input("Type the second number: "))
+        answer = operations[operator](n1, n2)
+        print(f"{n1} {operator} {n2} = {answer}")
+        #return operations[operator](n1, n2)
+
+        c = input("Do you want to keep working with the last number? Type 'y' or 'n' :")
+        if c == 'y':
+            n1 = answer
+        else:
+            loop = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
